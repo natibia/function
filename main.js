e7bb1b8630075ca401a26ba26834ee9ce4b0c990
natibia <142630925+natibia@users.noreply.github.com>
2024-10-04 08:30:49 -0300

    L

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-function soma (num1,num2){
-    return num1+num2;
-}
-console.log(soma(5,10));
+// function soma (num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(5,10));
 
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log(areaQuadrado(4));
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(4));
 
-let valorMedia1 = parseInt(prompt('insira valor 1'));
-let valorMedia2 = parseInt(prompt('insira valor 2'));
-let valorMedia3 = parseInt(prompt('insira valor 3'));
+// let valorMedia1 = parseInt(prompt('insira valor 1'));
+// let valorMedia2 = parseInt(prompt('insira valor 2'));
+// let valorMedia3 = parseInt(prompt('insira valor 3'));
 
-let media=function(num1,num2,num3){
-     return (num1+num2+num3)/3;
-}
-console.log(media(valorMedia1,valorMedia2,valorMedia3));
+// let media=function(num1,num2,num3){
+//      return (num1+num2+num3)/3;
+// }
+// console.log(media(valorMedia1,valorMedia2,valorMedia3));
+let estados = [
+    'AC','AL','AP', 'AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+
