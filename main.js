3adeb64bdd5157d3057a21190956963df1aceebf
natibia <142630925+natibia@users.noreply.github.com>
2024-10-04 09:11:12 -0300

    d

--- main.js
+++ main.js
@@ -18,9 +18,9 @@
 // console.log(media(valorMedia1,valorMedia2,valorMedia3));
 let estados = [
     'AC','AL','AP', 'AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
-let i = 0    
-    while (i<estados) {
-        console.log (i);
-        console.log (estados[i]);
-        i++
-    }
+// let i = 0    
+//     while (i<estados.length) {
+//         console.log (i);
+//         console.log (estados[i]);
+//         i++
+//     }
