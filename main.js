6184fc915847f4ee5171a90ead6e3625b809d7c1
natibia <142630925+natibia@users.noreply.github.com>
2024-10-04 09:24:26 -0300

    f

--- main.js
+++ main.js
@@ -16,15 +16,22 @@
 //      return (num1+num2+num3)/3;
 // }
 // console.log(media(valorMedia1,valorMedia2,valorMedia3));
-let estados = [
-    'AC','AL','AP', 'AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+// let estados = [
+//     'AC','AL','AP', 'AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 // let i = 0    
 //     while (i<estados.length) {
 //         console.log (i);
 //         console.log (estados[i]);
 //         i++
 //     }
-for (let index = 0; index < estados.length; index++) {
-    console.log (index);
-    console.log (estados[index]);    
+// for (let index = 0; index < estados.length; index++) {
+//     console.log (index);
+//     console.log (estados[index]);    
+// }
+let idade = 16;
+if (idade>17 ) {
+    console.log ('Pode dirigir');
 }
+else {
+    console.log ('Ilegal dirigir');
+} 
