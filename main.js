da0347c9cd74a53fe32d0151b3113a3e5eaee2e6
natibia <142630925+natibia@users.noreply.github.com>
2024-10-04 09:03:03 -0300

    f

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
 let estados = [
     'AC','AL','AP', 'AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 let i = 0    
-    while (i<10) {
+    while (i<27) {
         console.log (i);
         console.log (estados[i]);
         i++
